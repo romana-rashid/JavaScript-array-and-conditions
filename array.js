3cd46fc7b4976231bca52f2545db224564aaec66
Romana Rashid <romanacse98@gmail.com>
2022-01-26 21:07:27 +0600

    first commit

---
+++ array.js
@@ -0,0 +1,9 @@
+var friendsAge = [25, 37, 33, 26, 30];
+
+var muriChanachurPrices = [5, 6, 7, 8, 9];
+
+var heroines = ['karina', 'katrina', 'kiara', 'Divyanka'];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(heroines);
