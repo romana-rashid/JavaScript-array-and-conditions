39cbb6e285fcf258119ad50b62ca3be15fa8b57b
Romana Rashid <romanacse98@gmail.com>
2022-01-26 22:41:02 +0600

    unshift added

---
+++ push-pop.js
@@ -0,0 +1,22 @@
+//push => add element at the end of an array
+var lastBench = ['kalam', 'balam', 'alam'];
+lastBench.push('jamal');
+lastBench.push('palam');
+lastBench.push('sonam');
+console.log(lastBench);
+
+var friendsAge = [25, 37, 33, 26, 30];
+//friendsAge.push(50);
+//friendsAge.push(80);
+console.log(friendsAge);
+
+//pop => remove element from the end of an array
+
+friendsAge.pop();
+console.log(friendsAge);
+friendsAge.pop();
+console.log(friendsAge);
+
+var muriChanachurPrices = [5, 6, 7, 8, 9];
+muriChanachurPrices.unshift(10);
+console.log(muriChanachurPrices);
