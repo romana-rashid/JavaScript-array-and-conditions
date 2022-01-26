96ba726db0196ce5923a560885751b081034e3b0
Romana Rashid <romanacse98@gmail.com>
2022-01-26 21:28:53 +0600

    array index

---
+++ array-index.js
@@ -0,0 +1,15 @@
+var heroines = ['karina', 'katrina', 'kiara', 'Divyanka'];
+var indexOfHeroines = heroines.indexOf('kiara');
+console.log(indexOfHeroines);
+
+var numbers = [25, 37, 33, 26, 30];
+//var index = numbers.indexOf(26);
+var index = numbers.indexOf(126);
+console.log(index);
+
+var secondIndex = heroines[3];
+console.log(secondIndex);
+
+numbers[3] = 50;
+numbers[4] = 60;
+console.log(numbers);
